6e63ee322ebf3c33bf57190f684efe3876b106df
amaanansari26 <amn.github@gmail.com>
2023-03-27 12:46:21 +0530

    getAvailableMoves completed

--- src/Game.mjs
+++ src/Game.mjs
@@ -1,9 +1,10 @@
+/* eslint-disable no-case-declarations */
 class Game {
   board = {
     8: ['b-rook-1', 'b-knight-1', 'b-bishop-1', 'b-queen', 'b-king', 'b-bishop-2', 'b-knight-2', 'b-rook-2'],
     7: ['b-pawn-1', 'b-pawn-2', 'b-pawn-3', 'b-pawn-4', 'b-pawn-5', 'b-pawn-6', 'b-pawn-7', 'b-pawn-8'],
     6: [0, 0, 0, 0, 0, 0, 0, 0],
-    5: [0, 0, 0, 0, 0, 0, 0, 0],
+    5: [0, 0, 0, 'w-king', 0, 0, 0, 0],
     4: [0, 0, 0, 0, 0, 0, 0, 0],
     3: [0, 0, 0, 0, 0, 0, 0, 0],
     2: ['w-pawn-1', 'w-pawn-2', 'w-pawn-3', 'w-pawn-4', 'w-pawn-5', 'w-pawn-6', 'w-pawn-7', 'w-pawn-8'],
@@ -63,7 +64,7 @@ class Game {
 
   killedPieces = [];
 
-  getAvailableMoves(position) {
+  getAvailableMoves(position, isRecursion = false) {
     const row = Number(position.split('-')[0]);
     const pos = Number(position.split('-')[1]) - 1;
     const selectedPiece = this.board[row][pos];
@@ -71,8 +72,8 @@ class Game {
     const raceOfPiece = selectedPiece.split('-')[0];
     const opRaceOfPiece = raceOfPiece === 'w' ? 'b' : 'w';
     const classOfPiece = selectedPiece.split('-')[1];
-    const availableMoves = [];
-    const availableKillMoves = [];
+    let availableMoves = [];
+    let availableKillMoves = [];
     let pointer = this.board[row][pos];
     let prow = row;
     let ppos = pos;
@@ -172,6 +173,21 @@ class Game {
             availableKillMoves.push(`${krow}-${kpos + 1}`);
           }
         });
+        if (!isRecursion) {
+          const opRaceLocations = [];
+          for (let r = 1; r < 9; r += 1) {
+            for (let p = 0; p < 8; p += 1) {
+              if (this.board[r][p] !== 0 && this.board[r][p].split('-')[0] === opRaceOfPiece) opRaceLocations.push(`${r}-${p + 1}`);
+            }
+          }
+          let opAvailableMoves = [];
+          opRaceLocations.forEach((loc) => {
+            // eslint-disable-next-line max-len
+            opAvailableMoves = opAvailableMoves.concat(this.getAvailableMoves(loc, true).availableMoves);
+          });
+          availableMoves = availableMoves.filter((mov) => !opAvailableMoves.includes(mov));
+          availableKillMoves = availableKillMoves.filter((mov) => !opAvailableMoves.includes(mov));
+        }
 
         break;
       default:
@@ -189,5 +205,5 @@ class Game {
 export default Game;
 
 const myGame = new Game({ playerOneName: 'Amaan', playerTwoName: 'Hadiya' });
-console.log(myGame.getAvailableMoves('4-3'));
+console.log(myGame.getAvailableMoves('5-4'));
 console.log('done');
